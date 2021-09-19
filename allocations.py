effff8ebfdfccd473ead733672bc10600f27cfe8
TriumphTodd <todd.sassaman@gmail.com>
2021-09-19 00:01:30 -0400

    Fix joins and create reconciliation output

--- allocations.py
+++ allocations.py
@@ -7,10 +7,21 @@ This script will:
 
 import pandas as pd
 import numpy as np
+import os.path
+
+mapfile = 'mapccs.xlsx'
+calcfile = 'calculations.xlsx'
+reconfile = 'recon.xlsx'
+
+# Remove prior output files if they exist
+for x in [mapfile, calcfile, reconfile]: 
+    if os.path.exists(x): os.remove(x)
 
 # Load account data
+# NB:   The dataframe must have an index that uniquely identifies each row.  
+#       Otherwise the joins will only pick the first row of the matching columns.
 acctdf = pd.read_csv('accounts.csv')
-acctdf.set_index(['month', 'product', 'channel'])
+acctdf.set_index(['month', 'product', 'channel', 'account_num'])
 
 
 # Load expense data
@@ -24,7 +35,7 @@ mapdf.set_index(['month', 'cost_center'])
 
 # Join map with expenses
 mapccdf = pd.merge(expensedf, mapdf)
-mapccdf.to_excel('mapccs.xlsx')
+mapccdf.to_excel(mapfile)
 
 # Join mapped expenses with accounts (on month and product/channel)
 # get ccs mapped to products
@@ -40,7 +51,7 @@ channeldf.reset_index()
 channeldf = channeldf.drop(columns=['product_map'])        # just says 'all'
 channeldf.columns = ['month', 'cost_center', 'expense', 'maptype', 'channel', 'account_num', 'product', 'balance']
 
-# # append the two dataframnes
+# append the two dataframnes
 calcdf = pd.DataFrame()
 calcdf = calcdf.append(proddf)
 calcdf = calcdf.append(channeldf)
@@ -52,8 +63,16 @@ calcdf['total_balance'] = calcdf.groupby(['month', 'cost_center'])['balance'].tr
 calcdf['percent_balance'] = calcdf['balance'] / calcdf['total_balance']
 calcdf['allocated_expense'] = calcdf['expense'] * calcdf['percent_balance']
 
-# TODO: check for missing cost center costs (have accounts)
-
-
 # Export results
-calcdf.to_excel('calculations.xlsx', sheet_name='calculations')
+calcdf.to_excel(calcfile, sheet_name='calculations')
+
+# Reconcile missing cost center costs (have accounts)
+recondf = calcdf.loc[:,['month', 'cost_center', 'allocated_expense']]
+recondf['total_cc_allocated_expense'] = calcdf.groupby(['month', 'cost_center'])['allocated_expense'].transform('sum')
+recondf = recondf.drop(columns=['allocated_expense'])
+recondf = recondf.drop_duplicates()
+recondf = pd.merge(expensedf, recondf, on=['month', 'cost_center'], how='left', suffixes=['_expense', '_calc'])
+recondf['variance'] = recondf['total_cc_allocated_expense'] - recondf['expense']
+recondf.set_index(['month', 'cost_center'])
+recondf.to_excel(reconfile)
+
