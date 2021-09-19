c80bdf6bbc7286659a708a3890450c774c6b79e3
TriumphTodd <todd.sassaman@gmail.com>
2021-09-19 00:57:36 -0400

    Run pylint, fix formatting

--- allocations.py
+++ allocations.py
@@ -1,24 +1,25 @@
 """
 This script will:
  + read the input files
- + perform the expense allocations from the cost centers to the accounts based on their account balances
+ + perform the expense allocations from the cost centers to the
+   accounts based on their account balances
  + write out the results to an excel spreadsheet for reporting
 """
 
-import pandas as pd
-import numpy as np
 import os.path
+import pandas as pd
 
-mapfile = 'mapccs.xlsx'
-calcfile = 'calculations.xlsx'
-reconfile = 'recon.xlsx'
+MAP_FILE = 'mapccs.xlsx'
+CALC_FILE = 'calculations.xlsx'
+RECON_FILE = 'recon.xlsx'
 
 # Remove prior output files if they exist
-for x in [mapfile, calcfile, reconfile]: 
-    if os.path.exists(x): os.remove(x)
+for x in [MAP_FILE, CALC_FILE, RECON_FILE]:
+    if os.path.exists(x): 
+        os.remove(x)
 
 # Load account data
-# NB:   The dataframe must have an index that uniquely identifies each row.  
+# NB:   The dataframe must have an index that uniquely identifies each row.
 #       Otherwise the joins will only pick the first row of the matching columns.
 acctdf = pd.read_csv('accounts.csv')
 acctdf.set_index(['month', 'product', 'channel', 'account_num'])
@@ -35,28 +36,38 @@ mapdf.set_index(['month', 'cost_center'])
 
 # Join map with expenses
 mapccdf = pd.merge(expensedf, mapdf)
-mapccdf.to_excel(mapfile)
+mapccdf.to_excel(MAP_FILE)
 
 # Join mapped expenses with accounts (on month and product/channel)
 # get ccs mapped to products
 proddf = mapccdf[mapccdf['maptype']=='product']
-proddf = pd.merge(proddf, acctdf, on=['month', 'product'], how='left', suffixes=['_map', '_accts'])
+proddf = pd.merge(proddf, acctdf,
+    on=['month', 'product'], \
+    how='left',
+    suffixes=['_map', '_accts'])
 proddf = proddf.drop(columns=['channel_map'])        # just says 'all'
-proddf.columns = ['month', 'cost_center', 'expense', 'maptype', 'product', 'account_num', 'channel', 'balance']
+proddf.columns = ['month', 'cost_center', 'expense', 'maptype',
+    'product', 'account_num', 'channel', 'balance']
 
 # get ccs mapped to channels
 channeldf = mapccdf[mapccdf['maptype']=='channel']
-channeldf = pd.merge(channeldf, acctdf, on=['month', 'channel'], how='left', suffixes=['_map', '_accts'])
+channeldf = pd.merge(channeldf, acctdf,
+    on=['month', 'channel'],
+    how='left',
+    suffixes=['_map', '_accts'])
 channeldf.reset_index()
 channeldf = channeldf.drop(columns=['product_map'])        # just says 'all'
-channeldf.columns = ['month', 'cost_center', 'expense', 'maptype', 'channel', 'account_num', 'product', 'balance']
+channeldf.columns = ['month', 'cost_center', 'expense', 'maptype',
+    'channel', 'account_num', 'product', 'balance']
 
 # append the two dataframnes
 calcdf = pd.DataFrame()
 calcdf = calcdf.append(proddf)
 calcdf = calcdf.append(channeldf)
-calcdf.columns = ['month', 'cost_center', 'expense', 'maptype', 'product', 'account_num', 'channel', 'balance']
-calcdf = calcdf.loc[:, ['month', 'cost_center', 'expense', 'maptype', 'product', 'channel', 'account_num', 'balance']]      # reorder columns
+calcdf.columns = ['month', 'cost_center', 'expense', 'maptype',
+    'product', 'account_num', 'channel', 'balance']
+calcdf = calcdf.loc[:, ['month', 'cost_center', 'expense',
+    'maptype', 'product', 'channel', 'account_num', 'balance']]      # reorder columns
 
 # Calculate percentages
 calcdf['total_balance'] = calcdf.groupby(['month', 'cost_center'])['balance'].transform('sum')
@@ -64,15 +75,17 @@ calcdf['percent_balance'] = calcdf['balance'] / calcdf['total_balance']
 calcdf['allocated_expense'] = calcdf['expense'] * calcdf['percent_balance']
 
 # Export results
-calcdf.to_excel(calcfile, sheet_name='calculations')
+calcdf.to_excel(CALC_FILE, sheet_name='calculations')
 
 # Reconcile missing cost center costs (have accounts)
 recondf = calcdf.loc[:,['month', 'cost_center', 'allocated_expense']]
 recondf['total_cc_allocated_expense'] = calcdf.groupby(['month', 'cost_center'])['allocated_expense'].transform('sum')
 recondf = recondf.drop(columns=['allocated_expense'])
 recondf = recondf.drop_duplicates()
-recondf = pd.merge(expensedf, recondf, on=['month', 'cost_center'], how='left', suffixes=['_expense', '_calc'])
+recondf = pd.merge(expensedf, recondf,
+    on=['month', 'cost_center'],
+    how='left',
+    suffixes=['_expense', '_calc'])
 recondf['variance'] = recondf['total_cc_allocated_expense'] - recondf['expense']
 recondf.set_index(['month', 'cost_center'])
-recondf.to_excel(reconfile)
-
+recondf.to_excel(RECON_FILE)
--- create_inputs.py
+++ create_inputs.py
@@ -1,28 +1,30 @@
-
 """
 Create fake accounts for 9 products, 9 sales channels, 12 months, random AUM between 10**6 and 10**7
 """
 
-import numpy as np
-import itertools, csv, random
+import itertools
+import csv
+import random
 
 products = ["p" + str(p) for p in list(range(1,10)) ]
 channels = ["c" + str(c) for c in list(range(1,10)) ]
-centers = ["cc" + str(cc) for cc in list(range(1,10)) ]
+centers = ["cc" + str(cc) for cc in list(range(1,10))]
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 # -------------------------------------------------------------------------------------------------
 # Create account data
 # -------------------------------------------------------------------------------------------------
 
-# get cartesian product of months, products, channels and add a random account number and a random float for an account balance
+# get cartesian product of months, products, channels and add a random account number
+# and a random float for an account balance
 # https://stackoverflow.com/questions/1953194/permutations-of-two-lists-in-python
+
 accounts = []
-for x in itertools.product(months, products, channels): 
-    accounts.append((x[0], 
-                     'ac' + str(random.randint(10**3, 10**4)), 
-                     x[1], 
-                     x[2], 
+for x in itertools.product(months, products, channels):
+    accounts.append((x[0],
+                     'ac' + str(random.randint(10**3, 10**4)),
+                     x[1],
+                     x[2],
                      float(random.randint(10**6, 10**7))))
 
 # write out the list of tuples as a CSV file
@@ -33,10 +35,10 @@ with open('accounts.csv', 'w', newline='') as csvfile:
 
     writer.writeheader()
     for x in accounts:
-        writer.writerow({fieldnames[0]: x[0], 
-                         fieldnames[1]: x[1], 
-                         fieldnames[2]: x[2], 
-                         fieldnames[3]: x[3], 
+        writer.writerow({fieldnames[0]: x[0],
+                         fieldnames[1]: x[1],
+                         fieldnames[2]: x[2],
+                         fieldnames[3]: x[3],
                          fieldnames[4]: x[4]})
 
 # -------------------------------------------------------------------------------------------------
@@ -46,7 +48,8 @@ with open('accounts.csv', 'w', newline='') as csvfile:
 # get cartesian product of centers and months and add a random float for an expense number
 # https://stackoverflow.com/questions/1953194/permutations-of-two-lists-in-python
 expenses = []
-for x in itertools.product(months, centers): expenses.append((x[0], x[1], float(random.randint(10**4, 10**5))))
+for x in itertools.product(months, centers):
+    expenses.append((x[0], x[1], float(random.randint(10**4, 10**5))))
 
 # write out the list of tuples as a CSV file
 # https://docs.python.org/3/library/csv.html
@@ -70,7 +73,7 @@ with open('map.csv', 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
     writer.writeheader()
 
-    map = []
+    maplist = []
     # write out centers
     for c in centers:
         # write in 'all' in the client field if product, vice versa
@@ -79,13 +82,17 @@ with open('map.csv', 'w', newline='') as csvfile:
             mapdest = random.sample(products, k=random.randint(1,3))
             # create one row for each map destination with all in field 5
             for mapcc in itertools.product(months, [c], [maptype], mapdest, ['all']):
-                map.append(mapcc)
+                maplist.append(mapcc)
         else:
             mapdest = random.sample(channels, k=random.randint(1,3))
             # create one row for each map destination with all in field 4
             for mapcc in itertools.product(months, [c], [maptype], ['all'], mapdest):
-                map.append(mapcc)
+                maplist.append(mapcc)
     # write map
-    for m in map:
-        writer.writerow({fieldnames[0]: m[0], fieldnames[1]: m[1], fieldnames[2]: m[2], fieldnames[3]: m[3], fieldnames[4]: m[4]})
-
+    for m in maplist:
+        writer.writerow(
+            {fieldnames[0]: m[0],
+            fieldnames[1]: m[1],
+            fieldnames[2]: m[2],
+            fieldnames[3]: m[3],
+            fieldnames[4]: m[4]})
